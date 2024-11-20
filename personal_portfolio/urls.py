387a77fc9f3b0ec032f67f010c2c2868a4619211
raun1997 <raunakmitra7@gmail.com>
2024-11-20 13:20:17 +0530

    registered the static routes to our media files in urls.py

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # URL config for our pages app
     path('', include("pages.urls")),
     path("projects/", include("projects.urls")),
-]
+] +
+# to serve the media files 
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
