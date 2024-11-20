a8808f68e95eee291ac10c0573ce846758f1a57b
raun1997 <raunakmitra7@gmail.com>
2024-11-20 11:52:35 +0530

    created a project_index() function in projects/views.py

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+def project_index(request):
+    projects = Projects.objects.all() # fetching all the objects created
+    context = {
+        # Django uses the context dictionary to send information to your template.
+        "projects": projects
+    }
+    # Any entries in the context dictionary are available in the template, as long as you pass the context argument to render()
+    return render(request, "projects/projects_index.html", context)
