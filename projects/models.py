b02d2d41fb9ddb964f99e79c39a445995e883baa
raun1997 <raunakmitra7@gmail.com>
2024-11-20 13:14:22 +0530

    added an Image field for each project

--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     details = models.TextField()
     tech = models.CharField(max_length=20)
+    # define a FileField with a subfolder named project_images/.
+    # That’s where Django should store the images when you upload them.
+    image = models.FileField(upload_to="project_media/", blank=True)
