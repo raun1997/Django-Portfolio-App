90a7eed56ad281be6c14eb83080b698f4e2d92ef
raun1997 <raunakmitra7@gmail.com>
2024-11-20 13:03:49 +0530

    registered model in the Django admin site

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from projects.models import Project
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+# registering the model created by us
+admin.site.register(Project, ProjectAdmin)
