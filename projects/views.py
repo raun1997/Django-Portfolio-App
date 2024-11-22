1bc1932db9d193c3e54a2da066c3f3c37096095e
raun1997 <raunakmitra7@gmail.com>
2024-11-22 15:17:20 +0530

    added the link to the project_detail.html page

--- projects/views.py
+++ projects/views.py
@@ -11,7 +11,7 @@ def project_index(request):
     # Any entries in the context dictionary are available in the template, as long as you pass the context argument to render()
     return render(request, "projects/project_index.html", context)
 
-def project_details(request, pk):
+def project_detail(request, pk):
     project = Project.objects.get(pk=pk)  # grab a project by the primary key equal to function argument
     context = {
         # Django uses the context dictionary to send information to your template.
